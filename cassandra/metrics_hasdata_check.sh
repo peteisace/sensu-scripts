64bda74d550c797e7aa5e54a8db0bbe438b69814
pete <peteisace@gmail.com>
2021-11-02 20:32:47 +0700

    Add tail

--- cassandra/metrics_hasdata_check.sh
+++ cassandra/metrics_hasdata_check.sh
@@ -11,7 +11,7 @@ since_ms=$(($since_seconds*1000))
 current_date=$(($current_date-$since_ms))
 
 # now execute our query
-cqlout=`cqlsh -e "SELECT COUNT(*) FROM ts.metrics WHERE time >= minTimeUuid(${current_date}) ALLOW FILTERING"`
+cqlout=`cqlsh -e "SELECT COUNT(*) FROM ts.metrics WHERE time >= minTimeUuid(${current_date}) ALLOW FILTERING" | tail -n 11`
 echo "${cqlout}"
 
 count=`echo "${cqlout}" | grep -no "[0-9]\\+" | grep "4:" | sed -e "s/4://"`
